c13a238693ecf5f50cca736d7679ad112b6f7020
Brianna Shade <jarvis.brianna@gmail.com>
2014-07-05 13:58:11 -0700

    Comments and using tonality instead of modifier

--- markovChords/Chord.java
+++ markovChords/Chord.java
@@ -1,57 +1,94 @@
 package markovChords;
 
+/**
+ * "Digital Chords" Song Generator
+ * Copyright © 2014 Brianna Shade
+ * bshade@pdx.edu
+ *
+ * Chord.java
+ * TODO: details on this class
+ */
+
 import java.util.HashMap;
 
 public class Chord {
 
-	private HashMap<String, Integer> notes;
-	private String modifier;
+	//TODO: bitmap chord representation?
+	private HashMap<String, Integer> notes;			//stores actual notes of the chord
+	private String tonality;						//e.g.: major, minor, diminished, etc.
 	private int octave;								//can be + or -; defaults to middle
 	
-	public Chord(int relchord, String mod, int oct){
+	/**
+	 * Constructor method
+	 * @param relchord: numeral (integer) representation of relative Chord
+	 * @param tone: tonality of the Chord
+	 * @param oct: Chord's octave
+	 */
+	public Chord(int relchord, String tone, int oct){
 		int root = (relchord - 1) % 7;
 		notes.put("root", root);
 		
 		int third = root + 4;
-		switch (mod){
+		switch (tone){
 		case "major": third = root + 4; break;
 		case "minor":
 		case "diminished": third = root + 3; break;
-		default: System.err.println("Invalid modifier: " + mod + " (valid exaples: major, diminished)"); System.exit(1);
+		default: System.err.println("Invalid tonality: " + tone + " (valid exaples: major, diminished)"); System.exit(1);
 		}
 		notes.put("third", third);
 		
 		int fifth = root + 7;
-		if(mod.equals("diminished")) fifth = root + 6;
+		if(tone.equals("diminished")) fifth = root + 6;
 		notes.put("fifth", fifth);
 		
-		modifier = mod;
+		tonality = tone;
 		octave = oct;
 	}
 	
+	/**
+	 * Adjusts Chord to be of minor tonality
+	 */
 	public void makeMinor(){
 		notes.put("third", notes.get("root") + 3);
 		notes.put("fifth", notes.get("root") + 7);
 	}
 	
+	/**
+	 * Adjusts Chord to be of diminished tonality
+	 */
 	public void makeDiminished(){
 		notes.put("third", notes.get("root") + 3);
 		notes.put("fifth", notes.get("root") + 6);
 	}
 	
+	/**
+	 * Adjusts Chord to be of major tonality
+	 */
 	public void makeMajor(){
 		notes.put("third", notes.get("root") + 4);
 		notes.put("fifth", notes.get("root") + 7);
 	}
 	
+	/**
+	 * Adjust Chord's octave
+	 * @param oct: desired octave
+	 */
 	public void changeOctave(int oct){
 		octave = oct;
 	}
 	
+	/**
+	 * Retrieves Chord's notes
+	 * @return: the notes of the Chord
+	 */
 	public HashMap<String, Integer> getNotes(){
 		return notes;
 	}
 	
+	/**
+	 * Converts Chord into a string representation
+	 * @return: the string representation of the Chord
+	 */
 	@Override
 	public String toString(){
 		String chord = "{";
@@ -61,17 +98,25 @@ public class Chord {
 		return chord;
 	}
 	
+	/**
+	 * Automatically generated hashCode function for comparisons
+	 * @return: a hash value representing the Chord object
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result
-				+ ((modifier == null) ? 0 : modifier.hashCode());
+				+ ((tonality == null) ? 0 : tonality.hashCode());
 		result = prime * result + ((notes == null) ? 0 : notes.hashCode());
 		result = prime * result + octave;
 		return result;
 	}
 
+	/**
+	 * Automatically generated equals function for comparisons
+	 * @return: returns true iff obj = this
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -80,9 +125,9 @@ public class Chord {
 			return false;
 		
 		Chord other = (Chord) obj;
-		if (modifier == null && other.modifier != null)
+		if (tonality == null && other.tonality != null)
 			return false;
-		if (!modifier.equals(other.modifier))
+		if (!tonality.equals(other.tonality))
 			return false;
 		if (notes == null && other.notes != null)
 			return false;
