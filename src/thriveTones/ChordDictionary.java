fd5ac216d41ec6bcc8f009e23c3f54fa914e8170
Bart Massey <bart@cs.pdx.edu>
2014-09-30 17:38:11 -0700

    Cleaned up ChordDictionary put and added comments

--- src/thriveTones/ChordDictionary.java
+++ src/thriveTones/ChordDictionary.java
@@ -17,35 +17,47 @@ import java.util.Random;
 
 @SuppressWarnings("serial")
 public class ChordDictionary extends HashMap<LinkedList<Chord>, ArrayList<Chord>>{
-	private Random random_generator;
+	private Random random_generator = new Random();
 
-	public ChordDictionary(){
-		random_generator = new Random();
-	}
+	/**
+	 * Overloaded with native HashMap put
+	 * @param sequence: history string, hash key
+	 * @param chord: next chord possibility, hash value
+	 */
+	public void put(List<Chord> sequence, Chord chord){
+		// shallow copy the sequence for key storage
+		LinkedList<Chord> key = new LinkedList<Chord>(sequence);
 
-	public void put(LinkedList<Chord> sequence, Chord chord){
-		LinkedList<Chord> key = new LinkedList<Chord>();
-		if(sequence != null) key.addAll(sequence);					//make local copy to avoid it changing from under us
-
+		// add in this chord (even if already present for probability)
 		ArrayList<Chord> available_chords = this.get(key);
-		if(available_chords == null)
+		if(available_chords == null){
+			// first chord at this key
 			available_chords = new ArrayList<Chord>();
-		available_chords.add(chord);								//available chords might change, but put will overwrite anyway
-
-		this.put(key, available_chords);
+			this.put(key, available_chords);
+		}
+		available_chords.add(chord);								//available chords will change value component in dictionary
 
 		//add entire history
-		if(!key.isEmpty()){
-			LinkedList<Chord> seq = new LinkedList<Chord>();
-			if(key.size() > 1) seq.addAll(key.subList(1, key.size()));
-			put(seq, chord);
-		}
+		if(!key.isEmpty())
+			put(key.subList(1, key.size()), chord);
 	}
 
-	public Chord getANextChord(List<Chord> sequence) throws Exception{
+	/**
+	 * Given a Chord history set, returns a randomized next chord,
+	 * recursively looking at shorter histories if the current one
+	 * doesn't have a valid key value in the hash
+	 * @param sequence: Chord history to look up
+	 * @return: a valid next Chord
+	 */
+	public Chord getANextChord(List<Chord> sequence){
 		if(sequence == null)
 			sequence = new LinkedList<Chord>();
 		ArrayList<Chord> available_chords = this.get(sequence);
+		if(available_chords != null){
+			System.out.println("Available chords:");
+			for(Chord c : available_chords) System.out.print(c.toString() + " ");
+			System.out.println();
+		}
 		if(available_chords == null){
 			// history had nothing
 			assert(sequence.size() > 0);
@@ -54,7 +66,7 @@ public class ChordDictionary extends HashMap<LinkedList<Chord>, ArrayList<Chord>
 		}
 		//pull new random chord from the history choices
 		int index = random_generator.nextInt(available_chords.size());
-		System.out.println("Index: " + index);
+		System.out.println("Index: " + index + "/" + available_chords.size());
 		System.out.println("Chord: " + available_chords.get(index).toString());
 		return available_chords.get(index);
 	}
